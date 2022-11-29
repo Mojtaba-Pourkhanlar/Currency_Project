bffac411eb2a91c62168c013f16ca21655e40095
“[Mojtaba-Pourkhanlar]” <[mojtabapourkhanlar@gmail.com]>
2022-11-29 17:41:00 +0330

    set-Sidebar-04

--- src/frontEnd/components/afterLogin/components/Sidebar/tabComponents/DashboardTap/index.js
+++ src/frontEnd/components/afterLogin/components/Sidebar/tabComponents/DashboardTap/index.js
@@ -1,153 +1,16 @@
-import {
-  Box,
-  Grid,
-  Typography,
-  useTheme,
-  FormControl,
-  MenuItem,
-  Select,
-  InputLabel,
-  Button,
-  CircularProgress,
-} from "@mui/material";
-import { useContext, useEffect, useState } from "react";
-import { Line } from "react-chartjs-2";
-import { DasboardData } from "../../../../../../context/Dahsboard";
-import { HistoricalChart } from "../../../../../../services/api";
+import { t } from "i18next";
+import { useTranslation } from "react-i18next";
+import { Spinner } from "../../../../../../helpers";
 import { Header } from "../Header";
-import Chart from "chart.js/auto";
-import { CategoryScale } from "chart.js";
-Chart.register(CategoryScale);
-
-export const chartDays = [
-  {
-    label: "24 Hours",
-    value: 1,
-  },
-  {
-    label: "30 Days",
-    value: 30,
-  },
-  {
-    label: "3 Months",
-    value: 90,
-  },
-  {
-    label: "1 Year",
-    value: 365,
-  },
-];
 
 export const DashboardTap = () => {
-  const { coins, setLoading } = useContext(DasboardData);
-  const [allData, setAllData] = useState("");
-  const [chart, setChart] = useState([]);
-  const [stop, setStop] = useState(false);
-  const [days, setDays] = useState(1);
-
-  const id = coins
-    .filter((item) => item.name === allData)
-    .map((item) => item.id);
-
-  const fetchData = async () => {
-    const { data } = await HistoricalChart(id, days);
-    setLoading(true);
-    setChart(data.prices);
-  };
-
-  useEffect(() => {
-    if (allData !== "") {
-      fetchData();
-    }
-  }, [allData, days]);
-
-  const demo = {
-    "& .MuiSelect-select": {
-      color: "#fff",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "space-around",
-    },
-    "& svg": {
-      color: "#fff",
-    },
-  };
-
+  const { t } = useTranslation();
   return (
     <>
       <Header title="dashboard" subtitle="dashboardDec" />
 
-      <FormControl variant="standard" fullWidth>
-        <InputLabel id="demo-simple-select-label">
-          Please Selected Currency
-        </InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          label="Please Selected Currency"
-          sx={demo}
-          value={allData}
-          onChange={(e) => setAllData(e.target.value)}>
-          {coins.map((item) => (
-            <MenuItem key={item.id} value={item.name}>
-              <img src={item.image} width="40px" alt={item.name} />
-              <Typography variant="h5">{item.name}</Typography>
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-
-      <Line
-        data={{
-          labels: chart.map((coin) => {
-            let date = new Date(coin[0]);
-            let time =
-              date.getHours() > 12
-                ? `${date.getHours() - 12}:${date.getMinutes()} PM`
-                : `${date.getHours()}:${date.getMinutes()} AM`;
-            return days === 1 ? time : date.toLocaleDateString();
-          }),
-          datasets: [
-            {
-              data: chart.map((coin) => coin[1]),
-              label: `Price ( Past ${days} Days ) in `,
-              borderColor: "#EEBC1D",
-            },
-          ],
-        }}
-        options={{
-          elements: {
-            point: {
-              radius: 1,
-            },
-          },
-        }}
-      />
-      <div
-        style={{
-          display: "flex",
-          marginTop: 20,
-          justifyContent: "space-around",
-          width: "100%",
-        }}>
-        {chartDays.map((day) => (
-          <Button
-            key={day.value}
-            onClick={() => {
-              setDays(day.value);
-            }}
-            selected={day.value === days}>
-            {day.label}
-          </Button>
-        ))}
-      </div>
-
-      {/* {coin.map((item) => (
-        <div key={item.id}>
-          <img src={item.image} width="40px" alt={item.name} />
-          <Typography variant="h5">{item.name}</Typography>
-        </div>
-      ))} */}
+      <Spinner />
+      <h1>{t("coming")}</h1>
     </>
   );
 };
